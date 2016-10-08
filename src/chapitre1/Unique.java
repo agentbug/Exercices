fbde0a869f57b92dcbee8815b212f680efcba824
user <agentbug@gmail.com>
2016-10-08 23:40:05 +0200

    1.1

---
+++ src/chapitre1/Unique.java
@@ -0,0 +1,33 @@
+package chapitre1;
+
+
+
+public class Unique {
+	
+	
+	/**
+	 * Precondition: string non-null
+	 * postcondition: boolean
+	 * @param str
+	 * @return
+	 */
+	static boolean isUniqueChar(String str){
+		assert str == null :"the string is null";
+		
+		//if they have the same size, then str is unique
+		return str.chars().distinct().count() == str.length();
+		
+	}
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(isUniqueChar("test"));
+		
+		System.out.println(isUniqueChar("dsiuvcnh"));
+	}
+
+}
